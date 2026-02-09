c324431b9dba09f44c7f7c1939a593784a50247e
TornovDutta <tornovdutta@gmail.com>
2026-02-09 13:01:58 +0530

    fix it

--- src/pages/cart/CheckoutPage.js
+++ src/pages/cart/CheckoutPage.js
@@ -41,7 +41,7 @@ export const CheckoutPage = () => {
                     id: id
                 }
             }
-            const response = await fetch("http://localhost:8000/660/orders", {
+            const response = await fetch("http://localhost:8000/600/orders", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
@@ -79,7 +79,7 @@ export const CheckoutPage = () => {
                             </div>
                             <div className="mb-6">
                                 <label htmlFor="card" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Card Number (Mock)</label>
-                                <input type="text" id="card" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0000 0000 0000 0000" required minLength="16" />
+                                <input type="text" id="card" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0000 0000 0000 0000" required />
                             </div>
                             <div className="flex gap-4">
                                 <div className="mb-6 flex-1">
@@ -88,7 +88,7 @@ export const CheckoutPage = () => {
                                 </div>
                                 <div className="mb-6 flex-1">
                                     <label htmlFor="cvv" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">CVV</label>
-                                    <input type="text" id="cvv" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123" required minLength="3" maxLength="3" />
+                                    <input type="text" id="cvv" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123" required />
                                 </div>
                             </div>
                             <button type="submit" className="w-full text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-lg px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 transition duration-300">
--- src/pages/order/OrderSuccessPage.js
+++ src/pages/order/OrderSuccessPage.js
@@ -1,13 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export const OrderSuccessPage = () => {
+    const { state } = useLocation();
+    const data = state ? state.data : null;
+
     return (
         <main>
-            <section className="text-xl text-center max-w-4xl mx-auto my-10 py-5 dark:text-slate-100 border dark:border-slate-700 rounded">
+            <section className="text-xl text-center max-w-4xl mx-auto my-10 py-5 dark:text-slate-100 border dark:border-slate-700 rounded bg-white dark:bg-gray-800 shadow-md">
                 <div className="my-5">
                     <p className="bi bi-check-circle text-green-600 text-7xl mb-5"></p>
                     <p>Thank you so much for your order!</p>
-                    <p>Your order ID: {Math.floor(Math.random() * 1000000)}</p>
+                    {data ? (
+                        <>
+                            <p className="font-semibold text-lg mt-2">Your Order ID: {data.id}</p>
+                            <p className="text-base mt-1">Check your email <span className="font-semibold">{data.user.email}</span> for details.</p>
+                        </>
+                    ) : (
+                        <p className="text-red-500">Something went wrong accessing order details.</p>
+                    )}
                 </div>
                 <Link to="/products" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Continue Shopping <i className="ml-2 bi bi-cart"></i></Link>
             </section>
